fea20ba828cf914a441da275f125011f3037b77f
Samiya <skmcintosh20@gmail.com>
2022-06-04 14:04:54 -0700

    completed product list page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/Home/Home";
+import ProductList from "./pages/ProductList/ProductList";
 
 const App = () => {
 	return (
 		<div className="App">
-			<Home />
+			<ProductList />
 		</div>
 	);
 };
---
+++ src/pages/ProductList/ProductList.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Announcement from "../../components/Announcement";
+import Products from "../../components/Products";
+import Newsletter from "../../components/Newsletter";
+
+const ProductList = () => {
+	return (
+		<Container>
+			<Navbar />
+			<Announcement />
+			<Title>Active Wear</Title>
+			<FilterConatainer>
+				<Filter>
+					<FilterText>Filter Products:</FilterText>
+					<Select>
+						<Option disabled selected>
+							Color
+						</Option>
+						<Option>White</Option>
+						<Option>Blue</Option>
+						<Option>Gray</Option>
+						<Option>Red</Option>
+						<Option>Brown</Option>
+						<Option>Black</Option>
+					</Select>
+					<Select>
+						<Option disabled selected>
+							Size
+						</Option>
+						<Option>XS</Option>
+						<Option>S</Option>
+						<Option>M</Option>
+						<Option>L</Option>
+						<Option>XL</Option>
+					</Select>
+				</Filter>
+				<Filter>
+					<FilterText>Sort Products:</FilterText>
+					<Select>
+						<Option selected>Newest</Option>
+						<Option>Price (asc)</Option>
+						<Option>Price (desc)</Option>
+					</Select>
+				</Filter>
+			</FilterConatainer>
+			<Products />
+			<Newsletter />
+			<Footer />
+		</Container>
+	);
+};
+
+export default ProductList;
+
+const Container = styled.div``;
+const Title = styled.h1`
+	margin: 20px;
+`;
+
+const FilterConatainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+const Filter = styled.div`
+	margin: 20px;
+`;
+
+const FilterText = styled.span`
+	font-size: 20px;
+	font-weight: 600;
+	margin-right: 20px;
+`;
+
+const Select = styled.select`
+	padding: 10px;
+	margin-right: 20px;
+`;
+
+const Option = styled.option``;
